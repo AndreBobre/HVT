f767f45dd9f43e14954380eb5c6f55756d341671
Lars <galezkiy_1998@mail.ru>
2017-02-21 00:05:36 +0200

    different commit

--- Vht/src/ua/com/Vodovozov/ForFriend.java
+++ Vht/src/ua/com/Vodovozov/ForFriend.java
@@ -3,7 +3,7 @@ package ua.com.Vodovozov;
 public class ForFriend {
 
 	public static void main(String[] args) {
-		System.out.println("Nonono");
+		System.out.println("Nononoddfipndfklvndfjk");
 	}
 
 }
